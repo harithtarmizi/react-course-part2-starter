0fa3dae11dbfcdfaad393c3fe023d56ec485de5c
Rith <harith.trmz@gmail.com>
2023-06-29 13:10:50 +0800

    showing a loading indicator

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -10,8 +10,9 @@ interface Todo {
 
 const TodoList = () => {
   const fetchTodos = async () => {
-    const res = await axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos");
+    const res = await axios.get<Todo[]>(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
     return res.data;
   };
 
@@ -20,12 +21,18 @@ const TodoList = () => {
   // 2. has auto refetch
   // 3. first time get data, it store in cache
   // next want this data, can get directly from cache, no need from server
-  const { data: todos, error } = useQuery<Todo[], Error>({
+  const {
+    data: todos,
+    error,
+    isLoading,
+  } = useQuery<Todo[], Error>({
     // unique identifier, for caching purposes.
     queryKey: ["todos"],
     // function to fetch data from backend
     queryFn: fetchTodos,
   });
+
+  if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error.message}</p>;
 
