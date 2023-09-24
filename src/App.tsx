2b554877b1df5a31e4624fdad2a9f24509b88a94
Rith <harith.trmz@gmail.com>
2023-09-24 15:59:00 +0800

    creating a custom provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,18 @@ import taskReducer from "./state-management/reducers/tasksReducer";
 import NavBar from "./routing/NavBar";
 import HomePage from "./routing/HomePage";
 import TaskContext from "./state-management/contexts/tasksContext";
-import AuthContext from "./state-management/contexts/authContext";
-import authReducer from "./state-management/reducers/authReducer";
+import AuthProvider from "./state-management/AuthProvider";
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(taskReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, "");
 
   return (
-    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
       <TaskContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
         <NavBar />
         <HomePage />
       </TaskContext.Provider>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 
