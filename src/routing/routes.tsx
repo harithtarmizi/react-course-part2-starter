c548e81e8cb80b184f92a9f32a02178a9ecf1475
Rith <harith.trmz@gmail.com>
2023-09-26 01:38:25 +0800

    handling errors

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -3,11 +3,13 @@ import HomePage from "./HomePage";
 import UserDetailPage from "./UserDetail";
 import Layout from "./Layout";
 import UserPage from "./UserPage";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
